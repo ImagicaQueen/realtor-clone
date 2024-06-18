14eeca90bdd2092230147c44fcb6d5681525b5aa
khushichandravanshi <khushichandravansi3@gmail.com>
2024-06-18 16:15:30 +0530

    create signin signup forgot password pages and oauth component

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -1,56 +1,62 @@
 "use client";
 import React from "react";
 import { usePathname, useRouter } from "next/navigation";
+import { NavLinkProps } from "@/interfaces/header";
 
 const Header = () => {
   const pathname = usePathname();
   const router = useRouter();
 
   const pathMatchRoute = (route: string) => {
-    if (route === pathname) return true;
+    return route === pathname;
+  };
+
+  const NavbarMenu = ({ route, label }: NavLinkProps) => {
+    console.log("Route:", route);
+    console.log("Label:", label);
+    console.log("Pathname:", pathname);
+
+    const activeClass = pathMatchRoute(route)
+      ? "text-black border-b-red-500"
+      : "text-gray-400";
+
+    const handleClick = () => {
+      router.push(route);
+    };
+
+    return (
+      <>
+        <li
+          className={`cursor-pointer py-3 text-sm font-semibold ${activeClass} border-b-[3px] border-b-transparent`}
+          onClick={handleClick}
+        >
+          {label}
+        </li>
+      </>
+    );
   };
 
   return (
-    <div className="bg-white booder-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
             src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg"
             alt="Brand Logo"
             className="h-5 cursor-pointer"
-            onClick={() => router.push("/home")}
+            onClick={() => router.push("/")}
           />
         </div>
         <div>
           <ul className="flex space-x-10">
-            <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/home") && "text-black border-b-red-500 "
-              }`}
-              onClick={() => router.push("/home")}
-            >
-              Home
-            </li>
-            <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/offers") && "text-black border-b-red-500 "
-              }`}
-              onClick={() => router.push("/offers")}
-            >
-              About
-            </li>
-            <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/sign-in") && "text-black border-b-red-500 "
-              }`}
-              onClick={() => router.push("/sign-in")}
-            >
-              SignIn
-            </li>
+            <NavbarMenu route="/" label="Home" />
+            <NavbarMenu route="/offers" label="Offers" />
+            <NavbarMenu route="/sign-in" label="Sign In" />
           </ul>
         </div>
       </header>
     </div>
   );
 };
+
 export default Header;
--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -5,37 +5,20 @@ import React, { Fragment, useState } from "react";
 import { IoMdEye } from "react-icons/io";
 import { IoMdEyeOff } from "react-icons/io";
 
-const SignUp = () => {
-  const [showPassword, setShowPassword] = useState<boolean>(false);
-
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-  const { name, email, password } = formData;
+const ForgotPassword = () => {
+  const [email, setEmail] = useState<string>("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log(formData);
   };
 
-  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const { name, value } = e.currentTarget;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
   };
-
-  const handlePassword = () => {
-    setShowPassword((prevState) => !prevState);
-  };
-
   return (
     <Fragment>
       <section>
-        <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+        <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
         <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
           <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
             <img
@@ -47,15 +30,6 @@ const SignUp = () => {
           <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
             <form onSubmit={handleSubmit}>
               <input
-                type="name"
-                id="name"
-                name="name"
-                placeholder="Enter name"
-                value={name}
-                onChange={handleChange}
-                className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
-              />
-              <input
                 type="email"
                 id="email"
                 name="email"
@@ -64,41 +38,23 @@ const SignUp = () => {
                 onChange={handleChange}
                 className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
               />
-              <div className="relative mb-6">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={handleChange}
-                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
-                />
-                <div
-                  className="absolute right-3 top-3 text-xl cursor-pointer"
-                  onClick={() => {
-                    handlePassword();
-                  }}
-                >
-                  {showPassword ? <IoMdEye /> : <IoMdEyeOff />}
-                </div>
-              </div>
+
               <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
                 <p className="mb-6">
-                 Have an account?
+                  Don't have a account?
                   <Link
-                    href="/sign-in"
+                    href="/sign-up"
                     className="text-red-600 hover:text-red-700 text:sm transition duration-200 ease-in-out ml-1"
                   >
-                    Sign In
+                    Register
                   </Link>
                 </p>
                 <p>
                   <Link
-                    href="/forgot-password"
+                    href="/sign-in"
                     className="text-blue-600 hover:text-blue-800 text:sm transition duration-200 ease-in-out"
                   >
-                    Forgot password?
+                    Sign In instead
                   </Link>
                 </p>
               </div>
@@ -107,7 +63,7 @@ const SignUp = () => {
                 className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
                 type="submit"
               >
-                Sign Up
+                Set Reset Password
               </button>
               <div className="flex items-center my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
                 <p className="text-center font-semibold mx-4">OR</p>
@@ -121,4 +77,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default ForgotPassword;

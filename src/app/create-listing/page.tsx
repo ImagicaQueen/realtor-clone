63b58583a9e9d63d6a2dfb00f42f292ec1379e53
khushichandravanshi <khushichandravansi3@gmail.com>
2024-06-24 19:40:15 +0530

    modify create-listing page

--- src/app/create-listing/page.tsx
+++ src/app/create-listing/page.tsx
@@ -1,46 +1,20 @@
 "use client";
-import React, { MouseEvent, useState } from "react";
+import { CustomButtonProps, FormData } from "@/interfaces/create-listing";
+import React, { useState, MouseEvent, ChangeEvent } from "react";
 
-interface CustomButtonProps {
+export interface CustomInputProps {
   id: string;
-  value: string;
-  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
-  isActive: boolean;
-  children: React.ReactNode;
+  value: string | number;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  type: string;
+  placeholder?: string;
+  maxLength?: number;
+  minLength?: number;
+  required?: boolean;
+  className?: string;
+  min?: number;
+  max?: number;
 }
-interface FormData {
-  type: "rent" | "sale";
-  name: string;
-  bedrooms: number;
-  bathrooms: number;
-  parking: boolean;
-  furnished: boolean;
-  address: string;
-}
-// CustomButton Component
-const CustomButton: React.FC<CustomButtonProps> = ({
-  id,
-  value,
-  onClick,
-  isActive,
-  children,
-}) => {
-  return (
-    <button
-      type="button"
-      id={id}
-      value={value}
-      onClick={onClick}
-      className={`px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-        isActive ? "bg-white text-black" : "bg-slate-600 text-white"
-      }`}
-    >
-      {children}
-    </button>
-  );
-};
-
-// CreateListing Component
 const CreateListing = () => {
   const [formData, setFormData] = useState<FormData>({
     type: "rent",
@@ -50,13 +24,32 @@ const CreateListing = () => {
     parking: false,
     furnished: false,
     address: "",
+    description: "",
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    latitude: 0,
+    longitude: 0,
   });
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
 
-  const { type, name, bedrooms, bathrooms, parking, address, furnished } =
-    formData;
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    address,
+    furnished,
+    latitude,
+    longitude,
+    description,
+    regularPrice,
+    offer,
+  } = formData;
 
   const onChange = (event: any) => {
-    // setFormData({ ...formData, type: event.target.value });
+    setFormData({ ...formData, type: event.target.value });
     console.log("ttt");
   };
 
@@ -91,49 +84,50 @@ const CreateListing = () => {
             </div>
           </div>
           <p className="text-lg mt-6 font-semibold">Name</p>
-          <input
-            type="text"
+          <CustomInput
             id="name"
             value={name}
             onChange={onChange}
+            type="text"
             placeholder="Name"
             maxLength={32}
             minLength={10}
             required
-            className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
           />
           <div className="flex space-x-6 mb-6">
             <div>
               <p className="text-lg font-semibold">Beds</p>
-              <input
-                type="number"
+
+              <CustomInput
                 id="bedrooms"
                 value={bedrooms}
                 onChange={onChange}
-                min="1"
-                max="50"
+                type="number"
+                placeholder="Beds"
+                min={1}
+                max={50}
                 required
-                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                className="text-center"
               />
             </div>
             <div>
               <p className="text-lg font-semibold">Baths</p>
-              <input
-                type="number"
+              <CustomInput
                 id="bathrooms"
                 value={bathrooms}
                 onChange={onChange}
-                min="1"
-                max="50"
+                type="number"
+                placeholder="Baths"
+                min={1}
+                max={50}
                 required
-                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                className="text-center"
               />
             </div>
           </div>
           <p className="text-lg mt-6 font-semibold">Parking spot</p>
           <div className="flex">
-            <button
-              type="button"
+            <CustomButton
               id="parking"
               value="true"
               onClick={onChange}
@@ -142,9 +136,8 @@ const CreateListing = () => {
               }`}
             >
               Yes
-            </button>
-            <button
-              type="button"
+            </CustomButton>
+            <CustomButton
               id="parking"
               value="false"
               onClick={onChange}
@@ -153,12 +146,11 @@ const CreateListing = () => {
               }`}
             >
               no
-            </button>
+            </CustomButton>
           </div>
           <p className="text-lg mt-6 font-semibold">Furnished</p>
           <div className="flex">
-            <button
-              type="button"
+            <CustomButton
               id="furnished"
               value="true"
               onClick={onChange}
@@ -166,10 +158,10 @@ const CreateListing = () => {
                 !furnished ? "bg-white text-black" : "bg-slate-600 text-white"
               }`}
             >
-              yes
-            </button>
-            <button
-              type="button"
+              Yes
+            </CustomButton>
+
+            <CustomButton
               id="furnished"
               value="false"
               onClick={onChange}
@@ -178,8 +170,59 @@ const CreateListing = () => {
               }`}
             >
               no
-            </button>
+            </CustomButton>
           </div>
+          <p className="text-lg mt-6 font-semibold">Address</p>
+          <textarea
+            id="address"
+            value={address}
+            onChange={onChange}
+            placeholder="Address"
+            required
+            className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
+          />
+          {!geolocationEnabled && (
+            <div className="flex space-x-6 justify-start mb-6">
+              <div className="">
+                <p className="text-lg font-semibold">Latitude</p>
+
+                <CustomInput
+                  type="number"
+                  id="latitude"
+                  value={latitude}
+                  onChange={onChange}
+                  placeholder="Baths"
+                  min={-90}
+                  max={90}
+                  required
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center"
+                />
+              </div>
+              <div className="">
+                <p className="text-lg font-semibold">Longitude</p>
+                <input
+                  type="number"
+                  id="longitude"
+                  value={longitude}
+                  onChange={onChange}
+                  required
+                  min="-180"
+                  max="180"
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center"
+                />
+                <CustomInput
+                  type="number"
+                  id="longitude"
+                  value={longitude}
+                  onChange={onChange}
+                  min={-180}
+                  max={180}
+                  required
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center"
+                />
+              </div>
+            </div>
+          )}
         </form>
       </main>
     </>
@@ -187,3 +230,55 @@ const CreateListing = () => {
 };
 
 export default CreateListing;
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  id,
+  value,
+  onClick,
+  isActive,
+  children,
+  className = "",
+}) => {
+  return (
+    <button
+      type="button"
+      id={id}
+      value={value}
+      onClick={onClick}
+      className={`px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+        isActive ? "bg-white text-black" : "bg-slate-600 text-white"
+      } ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+const CustomInput: React.FC<CustomInputProps> = ({
+  id,
+  value,
+  onChange,
+  type,
+  placeholder,
+  maxLength,
+  minLength,
+  required = false,
+  className = "",
+  min,
+  max,
+}) => {
+  return (
+    <input
+      type={type}
+      id={id}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      maxLength={maxLength}
+      minLength={minLength}
+      required={required}
+      className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6 ${className}`}
+      min={min}
+      max={max}
+    />
+  );
+};
---
+++ src/interfaces/create-listing.ts
@@ -0,0 +1,23 @@
+export interface CustomButtonProps {
+  id: string;
+  value: any;
+  onClick?: (event: any) => void;
+  isActive?: boolean;
+  children: React.ReactNode;
+  className?: string;
+}
+export interface FormData {
+  type: "rent" | "sale";
+  name: string;
+  bedrooms: number;
+  bathrooms: number;
+  parking: boolean;
+  furnished: boolean;
+  address: string;
+  description: string;
+  offer: boolean;
+  regularPrice: number;
+  discountedPrice: number;
+  latitude: number;
+  longitude: number;
+}
